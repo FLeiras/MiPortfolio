f7ce9aa1b44a727883c3e9f24955b49c5a686692
FLeiras <fleiras18@gmail.com>
2022-08-08 20:03:17 -0300

    Inicio del Potfolio

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/header/Header";
+
+function WebContent() {
+  return (
+    <>
+      <div className="bg-circle1"></div>
+      <div className="bg-circle2"></div>
+      <Header />
+    </>
+  );
+}
+
+//export default WebContent;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <div id="home" className="container header-container">
+      <h3>
+        <p>
+          Hola <span>Soy</span>
+        </p>
+        Federico
+        <h2>Full Stack Developer con Orientación Front End</h2>
+      </h3>
+    </div>
+  );
+}
+
+export default Header;
