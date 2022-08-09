f7ee308f548bb71aed4e084a712b3c0a140a084b
FLeiras <fleiras18@gmail.com>
2022-08-09 10:13:15 -0300

    Nav reparada

---
+++ src/components/about/About.jsx
@@ -0,0 +1,11 @@
+import "./About.css";
+
+function About() {
+  return (
+    <div id="about" className="conatiner about-container">
+      <h1>Hola</h1>
+    </div>
+  );
+}
+
+export default About;
