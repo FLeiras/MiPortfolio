05d90c4679505738b62a5e92113889aa19ce1049
FLeiras <fleiras18@gmail.com>
2022-08-08 21:11:12 -0300

    NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
 
 function WebContent() {
   return (
     <>
       <div className="bg-circle1"></div>
       <div className="bg-circle2"></div>
+
       <Header />
+
+      <Nav />
     </>
   );
 }
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,44 @@
+import "./Nav.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { TiGroupOutline } from "react-icons/ti";
+import { BiMessasgeRoundedDots } from "react-icons/bi";
+import { BsArrowDownCircle } from "react-icons/bs";
+
+function Nav() {
+  return (
+    <div className="navigation">
+      <a href="#home">
+        <AiOutlineHome className="icon" />
+      </a>
+      <a href="#sobreMi">
+        <AiOutlineUser className="icon" />
+      </a>
+      <a href="#members">
+        <TiGroupOutline className="icon" />
+      </a>
+      <a href="#contacto">
+        <BiMessasgeRoundedDots className="icon" />
+      </a>
+      <a href="#footer">
+        <BsArrowDownCircle className="icon" />
+      </a>
+    </div>
+  );
+}
+
+let Icons = document.querySelectorAll(".navigation .icon");
+Icons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    changeactive();
+    icon.classList.add("active-nav");
+  });
+});
+
+function changeactive() {
+  Icons.forEach((icon) => {
+    icon.classList.remove("active-nav");
+  });
+}
+
+export default Nav;
